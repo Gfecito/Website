7f987a4c9adf1a742759aa630b29e853455a79f5
Gfecito <71032552+Gfecito@users.noreply.github.com>
2024-07-31 06:49:36 -0400

    Better styling on landing page

--- website/components/personal/Journey.vue
+++ website/components/personal/Journey.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="py-4 px-20 mx-20">
-    <h2 class="text-2xl font-bold">Personal Journey</h2>
-    <div class="space-y-4">
-      <p v-for="(one_liner,index) in data.one_liners" :key="one_liner">
+  <div class="py-10 px-20 mx-20">
+    <h2 class="text-2xl font-bold">Welcome! 👋</h2>
+    <div class="journey space-y-4">
+      <p v-for="(one_liner, index) in data.one_liners" :key="one_liner">
         <span class="font-bold">{{ one_liner }}</span>
         {{ data.paragraphs[index] }}
-      </p>  
+      </p>
     </div>
   </div>
 </template>
@@ -22,5 +22,13 @@ export default {
 </script>
 
 <style scoped>
-/* Add Tailwind CSS classes for component-specific styling */
+.journey {
+  max-width: 80%;
+  text-align: justify;
+  margin: auto;
+}
+
+.text-2xl {
+  padding-bottom: 50px;
+}
 </style>
